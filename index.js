bbd97e739bf8cd451ce630bfad466ea3c81df761
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-06-09 11:26:00 +0200

    Implement proper shutdown.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const process = require('process');
+
 const Koa = require('koa');
 const app = new Koa();
 
@@ -8,5 +10,18 @@ app.use(async ctx => {
 
 const port = 3000;
 
-console.log(`Server running on port ${port}`);
-app.listen(3000);
+const server = app.listen(3000, () => {
+    console.log(`Server running on port ${port} using node ${process.version}`);
+});
+
+process.on('SIGINT', onTerminate);
+process.on('SIGTERM', onTerminate);
+
+function onTerminate() {
+    console.log('Termination signal received, shutting down...');
+
+    server.close(() => {
+        console.log('Server stopped');
+        process.exit(0);
+    });
+}
