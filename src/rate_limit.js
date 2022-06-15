c8b1885e6094cf1fecfa359d24a08ff78af479b0
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-06-15 13:14:33 +0200

    Add html templating.

--- src/rate_limit.js
+++ src/rate_limit.js
@@ -1,16 +1,16 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as ejs from 'ejs'
+
 const maxTokens = 60;
 const tokenIncreaseEveryMs = 1000;
 
+const htmlView = fs.readFileSync(new URL('./view/rate_limit.ejs', import.meta.url));
+
+const htmlTemplate = ejs.compile(htmlView);
+
+
 let availableTokens = maxTokens;
-
-export async function rateLimit(ctx, next) {
-    if (availableTokens <= 0) {
-        ctx.throw(429, 'rate limit exceeded');
-    }
-
-    availableTokens--;
-    await next();
-}
 
 // Add token every `newTokenEveryMs` milliseconds, up to a maximum of `maxTokens`
 setInterval(function() {
@@ -18,3 +18,26 @@ setInterval(function() {
         availableTokens++;
     }
 }, tokenIncreaseEveryMs);
+
+
+export async function rateLimit(ctx, next) {
+    if (availableTokens <= 0) {
+        ctx.status = 429;
+        switch (ctx.accepts('html', 'json')) {
+            case 'html':
+                ctx.type = 'html';
+                ctx.body = htmlTemplate();
+                return;
+            case 'json':
+                ctx.type = 'json';
+                ctx.body = JSON.stringify({ error: 'Rate limit exceeded' });
+                return;
+            default:
+                return;
+        }
+    }
+
+    availableTokens--;
+    await next();
+}
+
