8be1a8df8e003e123062d14a2fa7291fa0029130
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-06-16 21:09:53 +0200

    Actually check for repository's existance when calling /package/:name/sync

--- src/package.js
+++ src/package.js
@@ -1,7 +1,41 @@
 import Router from '@koa/router'
+import * as childProcess from 'child_process'
+import { default as semver } from 'semver'
+import * as util from 'util'
+
 
 export const router = new Router();
 
+const execFile = util.promisify(childProcess.execFile);
+
+
 router.post('/package/:name/sync', async (ctx, next) => {
-    console.log(ctx.params.name);
+    const packageName = ctx.params.name;
+    const githubUrl = `https://github.com/${packageName}.git`;
+
+    try {
+        const { stdout } = await execFile('git', [ 'ls-remote', '--tags', githubUrl ], {
+            timeout: 3000
+        });
+
+        const entries = stdout
+            .trim()
+            .split('\n')
+            .map((entry) => entry.split('\t'))
+            .map(([hash, tag]) => {
+                const strippedTag = tag.replace('refs/tags/', '')
+                return [strippedTag, hash]
+            })
+            .filter(([tag, _]) => semver.valid(tag));
+
+        console.log(entries);
+    } catch (error) {
+        if (error.code === 128) {
+            console.log('Repository doesn\'t exist');
+        } else {
+            console.log(error);
+            ctx.throw(500);
+        }
+    }
 });
+
