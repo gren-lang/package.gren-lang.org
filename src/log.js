3906286d88cdcb00cc3ab7e54cab4af8133dfbd8
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-08-17 07:18:22 +0200

    Use pino for logging.

--- src/log.js
+++ src/log.js
@@ -1,20 +1,14 @@
+import { default as pino } from "pino";
+
+const defaultLogger = pino();
+
 export function info(msg, data) {
-  console.log(decorate("INFO", msg, data));
+  const logger = data ? defaultLogger.child(data) : defaultLogger
+  logger.info(msg);
 }
 
 export function error(msg, data) {
-  console.error(decorate("ERROR", msg, data));
-
-  if (data instanceof Error) {
-    console.error(data.stack);
-  }
+  const logger = data ? defaultLogger.child(data) : defaultLogger
+  logger.error(msg)
 }
 
-function decorate(level, msg, data) {
-  return JSON.stringify({
-    time: new Date().toISOString(),
-    level: level,
-    message: msg,
-    data: data,
-  });
-}
