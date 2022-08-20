71f576ef8846f1e55a12e7da93c6d2457b06cab4
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-08-20 20:26:39 +0200

    Add more metadata to the header, and correct favicons.

--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@ import process from "process";
 
 export const port = process.env["GREN_PORT"] || 3000;
 
+export const canonicalUrl = process.env["GREN_CANONICAL_URL"] || 'localhost';
+
 export const dbPath = process.env["GREN_PACKAGES_DATABASE"] || ":memory:";
 
 export const zulip = {
