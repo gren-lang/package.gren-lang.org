d9a50343bee0337027ffc2b7768027c865eb1d03
Robin Heggelund Hansen <git@heghan.org>
2024-04-28 19:48:35 +0200

    Pick up port from correct env variable

--- src/config.js
+++ src/config.js
@@ -1,16 +1,11 @@
 import process from "process";
 
-export const port = process.env["GREN_PORT"] || 3000;
+export const port = process.env["PORT"] || 3000;
 
 export const canonicalUrl =
   process.env["GREN_CANONICAL_URL"] || `http://localhost:${port}`;
 
 export const dbPath = process.env["GREN_PACKAGES_DATABASE"] || ":memory:";
-
-export const datadog = {
-  apiKey: process.env["DATADOG_API_KEY"],
-  appKey: process.env["DATADOG_APP_KEY"],
-};
 
 export const zulip = {
   username: process.env["GREN_ZULIP_USERNAME"],
