50e0fc8157d582159137cbdf90fda00aa41419fc
Robin Heggelund Hansen <git@heghan.org>
2026-03-23 20:27:36 +0100

    Add default.nix for making production builds

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  sources = import ./npins;
+  nixpkgs = import sources.nixpkgs { };
+  pkgJson = builtins.fromJSON (builtins.readFile ./package.json);
+in
+{ pkgs ? nixpkgs }:
+pkgs.buildNpmPackage {
+  pname = "package.gren-lang.org";
+  version = pkgJson.version;
+  src = ./.;
+  npmDepsHash = "sha256-uzUtbbWVqfCPDAZ4C9OHMx0/GQ4sraPkt5P3ZrvQBBg=";
+  makeCacheWritable = true; # to work around npm bug
+  dontNpmBuild = true;
+}
