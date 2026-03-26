613e5739b8dff593f15997492b2f5da8b092c421
Robin Heggelund Hansen <git@heghan.org>
2026-03-26 07:44:29 +0100

    Add meta to default.nix

--- default.nix
+++ default.nix
@@ -13,4 +13,11 @@ pkgs.buildNpmPackage {
   npmDepsHash = "sha256-pIpM5VJbPsef7wT3CJjv6aqGRwYGTx/F689x/miY6Tg=";
   makeCacheWritable = true; # to work around npm bug
   dontNpmBuild = true;
+
+  meta = {
+    description = "Package and documentation registry for Gren applications";
+    homepage = "https://packages.gren-lang.org";
+    license = pkgs.lib.licenses.bsd3;
+    mainProgram = "gren-package-server";
+  };
 }
